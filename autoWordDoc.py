cd0751f6c670aded0934f8e8ecf1d6ceea2cd46f
onera <charmSmartProgramming@gmail.com>
2023-12-12 16:37:37 +0800

    delete not necessary parts

--- autoWordDoc.py
+++
@@ -1,9 +0,0 @@
-from docx.api import Document
-
-document = Document(r"C:\Users\onera\OneDrive - ONE ERA (HK) LIMITED\雜\統一托盤，紙箱標識.docx")
-
-for table in document.tables:
-    print('new table')
-    for row in table.rows:
-        for cell in row.cells:
-            print(cell.text)
