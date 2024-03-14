7ed39b0c91b2c0dec7e36207a6aeb63991fec95e
onera <charmSmartProgramming@gmail.com>
2024-03-14 17:43:04 +0800

    added cs or onera in stock sheet

--- RefreshStock.py
+++ RefreshStock.py
@@ -23,11 +23,9 @@ cursor = connection.cursor()
 
 
 try:
-    excel = openpyxl.load_workbook(r"C:\Users\onera\OneDrive - ONE ERA (HK) LIMITED\oneraShare\庫存exportedStocks.xlsx")
+    excel = openpyxl.load_workbook(r"C:\Users\onera\OneDrive - ONE ERA (HK) LIMITED\oneraShare\庫存exportedStocks.xlsx", read_only=False)
     sheet = excel.active
-    sheet.title = 'sheet2'
-    excel.create_sheet('Sheet')
-    excel.remove(sheet)
+    sheet.delete_rows(2, sheet.max_row)
 except:
     excel = openpyxl.Workbook()
 sheet = excel.active
@@ -41,13 +39,13 @@ sheet = excel.active
 # colNum = len(records)
 # for i in records:
 #     sheet.cell(row = 1, column = records.index(i) + 1).value = i[0]
-l = ['型號', '數量', '發票', '現貨/退貨/盒', 'cost']
+l = ['型號', '數量', '發票', '現貨/退貨/盒', 'cost', 'cs/onera']
 
 for i in l:
     sheet.cell(row = 1, column = l.index(i)+1).value = i
 
 cursor.execute("""
-    select `型號`, `數量`, `發票`, `現貨/退貨/盒`, `cost` from `refInvoiceNo`
+    select `型號`, `數量`, `發票`, `現貨/退貨/盒`, `cost`, `cs/onera` from `refInvoiceNo`
     where `數量` <> 0 order by `型號`;
 """)
 records = cursor.fetchall()
