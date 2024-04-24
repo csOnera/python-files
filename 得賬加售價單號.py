b11b99a8112064858943636d6f1a65d5425b7598
onera <charmSmartProgramming@gmail.com>
2024-04-24 18:26:07 +0800

    m from old

--- 得賬加售價單號.py
+++ 得賬加售價單號.py
@@ -19,6 +19,12 @@ def searching(ref):
 for file in folder:
     filewb = openpyxl.load_workbook(file)
     filews = filewb['鉴定通过订单']
+
+    # for checking any not in 交易成功
+    fileNumberOfItem = filews.max_row - 3 #possibly 倉儲費
+    notFoundCount = 0
+    notFoundList = []
+
     for row in range(4,filews.max_row + 1):
         searchResult = searching(filews["b" + str(row)].value)
         if searchResult:
@@ -26,9 +32,14 @@ for file in folder:
             # ADD hkd price automatically
             ws["l" + str(searchResult)].value = filews['al' + str(row)].value
         elif filews["b" + str(row)].value != None:
-            print(filews["b" + str(row)].value + " not found")
+            # print(filews["b" + str(row)].value + " not found")
+            notFoundCount += 1
+            notFoundList.append(filews["b" + str(row)].value)
         else:
             print("none type detected")
+    print(file.name, "notFoundNumber訂單號找不到數量 (包括倉儲費):", notFoundCount, "下為找不到的訂單號")
+    for notFound in notFoundList:
+        print(notFound)
 
     filewb.save(file)
 
